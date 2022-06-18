bb26d5ab563dd3d9791194018131ab86c0170c26
Johan <j.a.denhartigh@gmail.com>
2022-06-18 17:14:22 +0200

    add cloudbuild.yml

--- scripts/repo-create.sh
+++ scripts/repo-create.sh
@@ -4,6 +4,7 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+helm version
 
 export HELM_EXPERIMENTAL_OCI=1
 gcloud auth print-access-token | helm registry login -u oauth2accesstoken \
@@ -14,9 +15,9 @@ echo "What is the name of the repo for this deployment?"
 repo="stuc-concurrent"
 echo "$repo"
 
-gcloud artifacts repositories create "$repo"-repo-2 \
---repository-format=docker \
---location=europe-west4
+#gcloud artifacts repositories create "$repo"-repo-2 \
+#--repository-format=docker \
+#--location=europe-west4
 
 git clone https://github.com/Johan1us/rasa-x-helm
 
@@ -30,3 +31,5 @@ helm package rasa-x
 
 
 helm push rasa-x-0.0.1.tgz oci://europe-west4-docker.pkg.dev/bot-studio-tech/stuc-concurrent-repo
+echo "repo is ..."
+echo "$repo"
