d90d7a00ec8965524cbfc601e07f625674d87895
Johan <j.a.denhartigh@gmail.com>
2022-06-18 17:27:21 +0200

    add cloudbuild.yml

--- scripts/repo-create.sh
+++ scripts/repo-create.sh
@@ -1,35 +1,30 @@
 #!/bin/sh
 # to activate: $bash script.sh
 
+helm version
+echo "Install HELM 3"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 helm version
 
+echo "Authenticate with an access token"
 export HELM_EXPERIMENTAL_OCI=1
 gcloud auth print-access-token | helm registry login -u oauth2accesstoken \
 --password-stdin https://europe-west4-docker.pkg.dev
 
-echo "What is the name of the repo for this deployment?"
-#read repo
-repo="stuc-concurrent"
-echo "$repo"
-
-#gcloud artifacts repositories create "$repo"-repo-2 \
-#--repository-format=docker \
-#--location=europe-west4
-
+echo "Clone git repo"
 git clone https://github.com/Johan1us/rasa-x-helm
 
 pushd rasa-x-helm/charts/rasa-x || exit
 
+echo "Update HELM dependencies"
 helm dependency update
 
 pushd ..
 
+echo "Package HELM Chart"
 helm package rasa-x
 
-
-helm push rasa-x-0.0.1.tgz oci://europe-west4-docker.pkg.dev/bot-studio-tech/stuc-concurrent-repo
-echo "repo is ..."
-echo "$repo"
+echo "Push HELM Chart"
+helm push rasa-x-0.0.1.tgz oci://europe-west4-docker.pkg.dev/bot-studio-tech/bot-studio-tech-repo
